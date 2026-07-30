a20419da325d8d5347d77eaa15c629e5532e0e67
vaibhav Laxman Takawane <vaibhavtakawane07@gmail.com>
2026-07-30 22:35:33 +0530

    cors handling

--- 06_CORS_Handling/Backend/main.py
+++ 06_CORS_Handling/Backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+#allowed origins (frontend url)
+origins = [
+    "http://localhost:5173",  # React app's origin
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      #allowed FE
+    allow_credentials=[True],   
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get("/")
 def home():
     return {"message": "Hello, World!"}
--- 06_CORS_Handling/Frontend/src/App.jsx
+++ 06_CORS_Handling/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
@@ -6,11 +6,27 @@ import { useState } from 'react'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log("ERROR FETCHING DATA : ", error))
+  }, [])
 
   return (
     <>
       <div> <h1>CORS Handling Demo</h1> This is a simple React app that demonstrates CORS handling with a FastAPI backend. </div>
+      <hr />
+      <div>
+        {
+          data ? (
+            <p>MESSAGE : {data.message}</p>
+          ) : (
+            <p>LOADING.....</p>)
+        }
+      </div>
     </>
   )
 }
